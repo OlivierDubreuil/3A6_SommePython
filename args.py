a8f29adbcb611a1cb0e7fbaaf80583839b672d79
6127521 <6127521@cstjean.qc.ca>
2020-11-04 14:53:44 -0500

    FIN args.py

--- args.py
+++ args.py
@@ -6,9 +6,18 @@ Programme pour afficher les arguments
 Par Olivier Dubreuil
 """
 
+import pprint
+import sys
+
 
 def main() -> None:
     """Fonction principale"""
+    pprint.pprint(sys.argv, width=40)
+    print()
+    print(f"Il u a {len(sys.argv) - 1} arguments")
+    for i, arg in enumerate(sys.argv):
+        print(f" - arg {i}: {arg}")
+
     pass
 
 
